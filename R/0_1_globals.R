bc665f77addfe0fbc953730de4a00c38e7b676b3
oscar <oscar@kjell>
2024-12-27 06:45:12 +0100

    cleaning files

--- R/0_1_globals.R
+++
@@ -1,8 +0,0 @@
-# Global settings (these avoids the note (warning) "no visible binding for global variable ‘XXX’
-# https://community.rstudio.com/t/how-to-solve-no-visible-binding-for-global-variable-note/28887
-utils::globalVariables(c(
-  # GENERAL
-  "find_talkrpp", "hgTransformerGetEmbedding",
-  "hgTransformerTranscribe"
-
-))
